ee4f357e3076073683451fe5d4eb7ecc9155a7be
Lydia <lreitzel@gmail.com>
2022-01-05 12:40:21 -0500

    added problem 2124 solution

---
+++ problem_2124.js
@@ -0,0 +1,16 @@
+// 2124. Check if All A's Appears Before All B's
+// Given a string s consisting of only the characters 'a' and 'b', return true if every 'a' appears before every 'b' in the string. Otherwise, return false.
+
+// My solution:
+
+var checkString = function(s) {
+    
+    for (let i = 0; i < s.length; i++) {
+        if (s.includes("ba")) {
+            return false
+        } else {
+            return true
+        }
+    }
+    
+};
