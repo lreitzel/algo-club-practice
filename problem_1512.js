d4e87a3e7e3411443185f672872dfedec7aea737
Lydia <lreitzel@gmail.com>
2022-01-19 23:37:14 -0500

    problem 1512 solution

---
+++ problem_1512.js
@@ -0,0 +1,18 @@
+// Number of Good Pairs
+// Given an array of integers nums, return the number of good pairs.
+// A pair (i, j) is called good if nums[i] == nums[j] and i < j.
+
+var numIdenticalPairs = function(nums) {
+    
+    let count = 0
+    
+    for(let i = 0; i < nums.length; i++){
+        for(let j = i + 1; j < nums.length; j++){
+            if(nums[i] == nums[j]){
+                count++
+            }
+        }
+    }
+    return count;
+    
+};
