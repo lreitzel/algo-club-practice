a3b2c9201fd237d24e7510ecbbad069f42f929b5
Lydia <lreitzel@gmail.com>
2022-01-27 23:57:41 -0500

    removed unnecessary items

--- problem_1512.js
+++ problem_1512.js
@@ -2,10 +2,8 @@
 // Given an array of integers nums, return the number of good pairs.
 // A pair (i, j) is called good if nums[i] == nums[j] and i < j.
 
-var numIdenticalPairs = function(nums) {
-    
+var numIdenticalPairs = function(nums) { 
     let count = 0
-    
     for(let i = 0; i < nums.length; i++){
         for(let j = i + 1; j < nums.length; j++){
             if(nums[i] == nums[j]){
@@ -14,5 +12,4 @@ var numIdenticalPairs = function(nums) {
         }
     }
     return count;
-    
 };
